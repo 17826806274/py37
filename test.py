b83e30c4f08d3452b706b63f28758653de781e81
wzf <wzfqaz789@qq.com>
2019-03-31 21:47:27 +0800

    connect aliyun mysql

--- test.py
+++ test.py
@@ -7,11 +7,9 @@ import time
 
 res = requests.get('http://www.runoob.com/')
 res.raise_for_status()
-res.encoding='utf-8'
+res.encoding = 'utf-8'
 bs = bs4.BeautifulSoup(res.text,"html5lib")
 h4_list = bs.select('h4')
-
-rex1 = "(?<=【).*?(?=】)"
 
 def write_file(txt):
     f1 = open('web4.txt','a',encoding='utf8')
@@ -26,7 +24,6 @@ def local_time():
 for h4 in bs.find_all('h4'):
     matchobj = re.match(r'【',h4.string)
     if matchobj:
-        #print(h4.string)
         time1 = local_time()
         write_file(time1 +' '+ h4.string + '\n' )
 
